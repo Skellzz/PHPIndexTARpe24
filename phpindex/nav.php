2ad0b2b38450e520627d776ff18f5cfc2cfd316f
Skellzz <163452729+Skellzz@users.noreply.github.com>
2025-11-26 16:43:32 +0200

    Uuendasin Nav.php ja lisasin dropdown menu

--- phpindex/nav.php
+++ phpindex/nav.php
@@ -1,26 +1,23 @@
-<nav>
-    <ul>
-        <li>
-            <a href="?leht=kodu.php">Kodu</a>
-        </li>
-        <li>
-            <a href="?leht=jsToo.php">JS joonis</a>
-        </li>
-        <li>
-            <a href="?leht=Ajafunktsioonid.php">Ajafunktsioonid</a>
-        </li>
-        <li>
-            <a href="?leht=Kalkulaator.php">Kalkulaator</a>
-        </li>
-        <li>
-            <a href="?leht=matemaatilised.php">Matemaatilised Funktsioonid</a>
-        </li>
-        <li>
-            <a href="https://georgeteemus24.thkit.ee" taget="_blank">Vana Index veebileht</a>
-        </li>
-        <li>
-            <a href="?leht=gitKasud.php">gitKasud</a>
-        </li>
-    </ul>
-</nav>
-
+<!DOCTYPE html>
+<html lang="et">
+<body>
+<div id="main">
+    <div id="content">
+        <nav class="menu">
+            <ul>
+                <li><a href="?leht=kodu.php">Kodu</a></li>
+                <li><a href="https://georgeteemus24.thkit.ee" target="_blank">Vana Index</a></li>
+                <li><a href="?leht=gitKasud.php">gitKasud</a></li>
+                <li><a href="#">FUNKTSIOONID</a>
+                    <ul class="dropdown">
+                        <li><a href="?leht=Ajafunktsioonid.php">Ajafunktsioonid</a></li>
+                        <li><a href="?leht=Kalkulaator.php">Kalkulaator</a></li>
+                        <li><a href="?leht=matemaatilised.php">Matemaatilised Funktsioonid</a></li>
+                    </ul>
+                </li>
+            </ul>
+        </nav>
+    </div>
+</div>
+</body>
+</html>
